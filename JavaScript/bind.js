9cdfab0e90dffddf75375020f0168a1f03906681
wangmaoquan <1782976211@qq.com>
2023-05-28 17:11:10 +0800

    fix: 完善call

--- JavaScript/bind.js
+++ JavaScript/bind.js
@@ -2,6 +2,11 @@
  * bind 方法创建一个新的函数
  * 在 bind() 被调用时，这个新函数的 this 被指定为 bind() 的第一个参数，而其余参数将作为新函数的参数，供调用时使用
  * 如果使用 new 运算符构造绑定函数，则忽略该值(即第一个参数)
+ *
+ * 调用绑定函数时作为 this 参数传递给目标函数的值。
+ * 如果使用new运算符构造绑定函数，则忽略该值。当
+ * 使用 bind 在 setTimeout 中创建一个函数（作为回调提供）时，作为 thisArg 传递的任何原始值都将转换为 object。
+ * 如果 bind 函数的参数列表为空，或者thisArg是null或undefined，执行作用域的 this 将被视为新函数的 thisArg
  */
 
 Function.prototype.myBind = function (context, ...bindArgs) {
